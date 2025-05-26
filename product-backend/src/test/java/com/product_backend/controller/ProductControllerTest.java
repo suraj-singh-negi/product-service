25f105304c6debd6aa05ac0c3d2a8312028882bd
suraj-singh-negi <75073846+suraj-singh-negi@users.noreply.github.com>
2025-05-26 15:45:55 +0000

    added controller junit

---
+++ product-backend/src/test/java/com/product_backend/controller/ProductControllerTest.java
@@ -0,0 +1,13 @@
+package com.product_backend.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProductControllerTest {
+    
+    @InjectMocks
+    private ProductController productController;
+
+}
